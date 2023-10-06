8280267e46858082269c0a84f1d82648f44e3834
HoangPham2704 <147042536+HoangPham2704@users.noreply.github.com>
2023-10-06 12:48:24 +0700

    Update Case_Study.R

--- Case_Study.R
+++ Case_Study.R
@@ -34,11 +34,11 @@ library(lubridate)
 
 #3. Making sure that the the columns have the same type
 
-> compare_df_cols(jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,sep,return = "mismatch")
+> compare_df_cols(jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec,return = "mismatch")
 
 #4. Combining Data sets
 
-> bike_rides_2022 <- rbind(jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,sep)
+> bike_rides_2022 <- rbind(jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec)
 > dim(bike_rides_2022)
 
 #5. Removing empty rows and columns then check if any was removed
@@ -53,24 +53,19 @@ library(lubridate)
 
 #7. Processing datetime, and creating start and end hour fields
 
-> bike_rides_2022 <- bike_rides_2022 %>% mutate_at(vars(started_at, ended_at), ymd_hms)
-> bike_rides_2022 <- bike_rides_2022 %>% mutate_at(vars(started_at, ended_at), hour)
+> bike_rides_2022 <- bike_rides_2022 %>% mutate(started_at_datetime = ymd_hms(started_at), ended_at_datetime = ymd_hms(ended_at))
+> bike_rides_2022 <- bike_rides_2022 %>% mutate(started_hour = hour(started_at), ended_hour = hour(ended_at))
+
 
 #8. Creating ride_length field
 
-> bike_rides_2022 <- bike_rides_2022 %>%
-     mutate(
-             ride_length_hours = as.numeric(difftime(ended_at, started_at, units = "hours")),
-             ride_length_mins = as.numeric(difftime(ended_at, started_at, units = "mins"))
-           )
+> bike_rides_2022 <- bike_rides_2022 %>% mutate(ride_length_hours = as.numeric(difftime(ended_at, started_at, units = "hours")))
+> bike_rides_2022 <- bike_rides_2022 %>% mutate(ride_length_mins = as.numeric(difftime(ended_at, started_at, units = "mins")))
 
 #9. Creating day_of_the_week fields
 
-> bike_rides_2022 <- bike_rides_2022 %>%
-      mutate(
-              day_of_week_letter = wday(started_at, abbr = TRUE, label = TRUE),
-              day_of_week_number = wday(started_at)
-            )
+> bike_rides_2022 <- bike_rides_2022 %>% mutate(day_of_week_letter = wday(started_at, abbr = TRUE, label = TRUE))
+> bike_rides_2022 <- bike_rides_2022 %>% mutate(day_of_week_number = wday(started_at))
 
 #10. summary of data
 
@@ -79,12 +74,14 @@ library(lubridate)
 #11. Removing na and duplicate
 
 > bike_rides_2022_no_na <- bike_rides_2022 %>% drop_na()
-
 > bike_rides_2022_no_na <- distinct(bike_rides_2022_no_na)
+> rm(bike_rides_2022)
 
 #12. Removing negative ride length
 
 > bike_rides_2022_done <- bike_rides_2022_no_na %>% filter(ride_length_mins>0)
+> rm(bike_rides_2022_no_na)
+> rm(jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec)
 
 #13. summary of data
 
